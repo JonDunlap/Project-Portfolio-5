ae5c8ef98d0181fb45b19dc729ff79c645d2d296
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-02-12 15:20:58 +0700

    feat: :sparkles: complete basic functionality of search component
    Competed the basic functionality of the search component allowing the user to select a predetermined region, state, city so that development can continue on the other components.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,18 +1,17 @@
 import { useState } from 'react';
 import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
 import FormControl from '@material-ui/core/FormControl';
+import Grid from '@material-ui/core/Grid';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Paper from '@material-ui/core/Paper';
 import Select from '@material-ui/core/Select';
-import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(2),
-    minWidth: 200,
+    minWidth: 250,
   },
   selectEmpty: {
     marginTop: theme.spacing(2),
@@ -20,49 +19,119 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 // Search component
-const Search = ({ submitForm, setLocName, location }) => {
+const Search = ({ submitForm, setLocName }) => {
+  // Styling
   const classes = useStyles();
+  // State variables
   const [region, setRegion] = useState('');
+  const [state, setState] = useState('');
+  const [city, setCity] = useState('');
+  // Variables used to enable/disable inputs/buttons
+  let stateDisabled = !region;
+  let cityDisabled = !state;
+  let btnDisabled = !city;
 
-  const handleChange = (e) => {
+  // onChange functions to set state
+  const handleRegionChange = (e) => {
     setRegion(e.target.value);
+  };
+  const handleStateChange = (e) => {
+    setState(e.target.value);
+  };
+  const handleCityChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  //
+  const handleSubmit = (e) => {
+    // Prevent default submit
+    e.preventDefault();
+
+    // Create location object
+    // TODO - temp location
+    setLocName(city);
+
+    // Submit the form to the main app
+    submitForm();
+
+    // Clear the form fields
+    setRegion('');
+    setState('');
+    setCity('');
   };
 
   return (
     <Paper variant='outlined' elevation={3} className='search-container'>
-      <form onSubmit={submitForm}>
-        <FormControl className={classes.formControl}>
-          <InputLabel id='demo-simple-select-required-label'>Region</InputLabel>
-          <Select
-            labelId='demo-simple-select-required-label'
-            id='demo-simple-select-required'
-            value={region}
-            onChange={handleChange}
-            autoWidth
-            className={classes.selectEmpty}
-          >
-            {/* TODO - map through the regions */}
-            <MenuItem value=''>
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
-        </FormControl>
-        <TextField
-          className={classes.formControl}
-          id='standard-required'
-          label='City'
-        />
-        <Button
-          className={classes.formControl}
-          variant='contained'
-          disabled
-          onClick={submitForm}
+      <form>
+        <Grid
+          container
+          spacing={2}
+          justify='center'
+          align='center'
+          width='auto'
         >
-          Search
-        </Button>
+          <Grid item sm={12} md={6} lg={3}>
+            <FormControl className={classes.formControl}>
+              <InputLabel id='region-label'>Region</InputLabel>
+              <Select
+                labelId='region-label'
+                id='region'
+                value={region}
+                onChange={handleRegionChange}
+                autoWidth
+                className={classes.selectEmpty}
+              >
+                {/* TODO - map through the regions? */}
+                <MenuItem value={'US'}>United States of America</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item sm={12} md={6} lg={3}>
+            <FormControl className={classes.formControl}>
+              <InputLabel id='state-label'>State</InputLabel>
+              <Select
+                disabled={stateDisabled}
+                labelId='state-label'
+                id='state'
+                value={state}
+                onChange={handleStateChange}
+                autoWidth
+                className={classes.selectEmpty}
+              >
+                {/* TODO - map through the states */}
+                <MenuItem value={'NV'}>Nevada</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item sm={12} md={6} lg={3}>
+            <FormControl className={classes.formControl}>
+              <InputLabel id='city-label'>City</InputLabel>
+              <Select
+                disabled={cityDisabled}
+                labelId='city-label'
+                id='city'
+                value={city}
+                onChange={handleCityChange}
+                autoWidth
+                className={classes.selectEmpty}
+              >
+                {/* TODO - map through the cities */}
+                <MenuItem value={'las vegas'}>Las Vegas</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item sm={12} md={6} lg={3}>
+            <Button
+              disabled={btnDisabled}
+              className={classes.formControl}
+              id='search-btn'
+              variant='contained'
+              onClick={handleSubmit}
+            >
+              Search
+            </Button>
+          </Grid>
+        </Grid>
       </form>
     </Paper>
   );
