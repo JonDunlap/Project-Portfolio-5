6468a6aed16fc10541b2d004cfb004fa1dd252ed
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-03-01 00:01:43 +0700

    refactor: :art: move variables & functions used in search to the search component
    Move all the variables and functions that are used only in the search component into the search component to help clean up the App component and increase performance.

--- src/App.js
+++ src/App.js
@@ -1,171 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Main from './pages/Main';
 import Search from './pages/Search';
-
-// TODO - move to search component outside of the constructor function
-//* Header and options used for the country-city-state API
-const headers = new Headers();
-headers.append(
-  'X-CSCAPI-KEY',
-  'Tmk3R2VmamRvWEsxbUpHREFSWHlSRW5pZ050Q2QwMVBPdjRTdGFVRA=='
-);
-const requestOptions = {
-  method: 'GET',
-  headers: headers,
-  redirect: 'follow',
-};
 
 function App() {
   //* State variables used in the App component
   const [location, setLocation] = useState();
-  // const [location, setLocation] = useState({
-  //   lat: '36.1699412',
-  //   lng: '-115.1398296',
-  // }); //! DEBUG - manually set location to skip search component
   const [cityState, setCityState] = useState();
-  // const [cityState, setCityState] = useState({
-  //   city: 'Las Vegas',
-  //   state: 'NV',
-  // }); //! DEBUG - manually set city & state to skip search component
   const [weather, setWeather] = useState();
-
-  // TODO - move to search component
-  //* State variables used in search component
-  const [region, setRegion] = useState('');
-  const [state, setState] = useState('');
-  const [city, setCity] = useState('');
-  const [regionList, setRegionList] = useState();
-  const [stateList, setStateList] = useState();
-  const [cityList, setCityList] = useState();
-
-  //* Fetch the region list on initial load of the App component
-  useEffect(() => {
-    const fetchRegion = async () => {
-      await fetch(
-        'https://api.countrystatecity.in/v1/countries',
-        requestOptions
-      )
-        .then((response) => response.json())
-        .then((result) => setRegionList(result))
-        .catch((error) => console.log('error', error));
-    };
-
-    fetchRegion();
-  }, []);
-  //* Fetch the state list after the region has been set
-  useEffect(() => {
-    const fetchState = async () => {
-      await fetch(
-        `https://api.countrystatecity.in/v1/countries/${region}/states`,
-        requestOptions
-      )
-        .then((response) => response.json())
-        .then((result) => setStateList(result))
-        .catch((error) => console.log('error', error));
-    };
-
-    if (region) {
-      fetchState();
-    }
-  }, [region]);
-  //* Fetch the city list after the state has been set
-  useEffect(() => {
-    const fetchCity = async () => {
-      await fetch(
-        `https://api.countrystatecity.in/v1/countries/${region}/states/${state}/cities`,
-        requestOptions
-      )
-        .then((response) => response.json())
-        .then((result) => setCityList(result))
-        .catch((error) => console.log('error', error));
-    };
-
-    if (state) {
-      fetchCity();
-    }
-  }, [state, region]);
-  //* Fetch the weather data after the lat lng has been retrieved from the Google API
-  useEffect(() => {
-    // Async function to get weather data for the users input location
-    const fetchWeather = async () => {
-      await fetch(
-        `https://api.openweathermap.org/data/2.5/onecall?lat=${location.lat}&lon=${location.lng}&exclude=minutely&appid=${process.env.REACT_APP_API_KEY}`
-      )
-        .then((response) => response.json())
-        // Call the function to set the weather state
-        .then((weatherData) => setWeather({ ...weatherData }))
-        // Handle any errors
-        .catch((error) => console.log(`Error: ${error}`));
-    };
-
-    if (location) {
-      fetchWeather();
-    }
-  }, [location]);
-
-  //* onChange functions to set state
-  const handleRegionChange = (e) => {
-    setRegion(e.target.value);
-  };
-  const handleStateChange = (e) => {
-    setState(e.target.value);
-  };
-  const handleCityChange = (e) => {
-    setCity(e.target.value);
-  };
-
-  //* Handle the form submission
-  const submitForm = (e) => {
-    e.preventDefault();
-
-    setCityState({ city, state });
-    // fetch lat lng location from google API
-    getLatLng();
-
-    // Clear the search form fields by clearing the state variables
-    setRegion('');
-    setState('');
-    setCity('');
-  };
-
-  //* Get latitude & longitude from address using google maps geocode API
-  const getLatLng = async () => {
-    await fetch(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${city},+${state},+${region}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`
-    )
-      .then((response) => response.json())
-      .then((data) => data.results[0].geometry.location)
-      .then((latLng) => setLocation({ ...latLng }))
-      .catch((error) => console.error(error));
-  };
 
   return (
     <div className='container'>
-      {/* Check if the weather state has data, if not show the search component, if it does show the main weather component */}
+      {/* //* Check if the weather state has data, if it does show the main weather component, if not show the search component */}
       {weather ? (
         <Main weather={weather} cityState={cityState} />
-      ) : regionList ? (
-        // TODO - move the regionList check into the search component along with loading
+      ) : (
         <Search
-          // TODO - move these to search component
-          region={region}
-          state={state}
-          city={city}
-          handleRegionChange={handleRegionChange}
-          handleStateChange={handleStateChange}
-          handleCityChange={handleCityChange}
-          regionList={regionList}
-          stateList={stateList}
-          cityList={cityList}
-          submitForm={submitForm}
-          // TODO - keep these
+          location={location}
           setLocation={setLocation}
           setCityState={setCityState}
           setWeather={setWeather}
         />
-      ) : (
-        // TODO - Create a loading component while waiting for regions
-        'Loading'
       )}
     </div>
   );
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -7,6 +7,19 @@ import Paper from '@material-ui/core/Paper';
 import Select from '@material-ui/core/Select';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import { useState, useEffect } from 'react';
+
+//* Header and options used for the country-city-state API
+const headers = new Headers();
+headers.append(
+  'X-CSCAPI-KEY',
+  'Tmk3R2VmamRvWEsxbUpHREFSWHlSRW5pZ050Q2QwMVBPdjRTdGFVRA=='
+);
+const requestOptions = {
+  method: 'GET',
+  headers: headers,
+  redirect: 'follow',
+};
 
 //* Styles used for customizing the material-ui components
 const useStyles = makeStyles((theme) => ({
@@ -24,121 +37,233 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 //* Search component with props
-const Search = ({
-  region,
-  state,
-  city,
-  submitForm,
-  handleRegionChange,
-  handleStateChange,
-  handleCityChange,
-  regionList,
-  stateList,
-  cityList,
-}) => {
-  // Variables
+const Search = ({ location, setLocation, setCityState, setWeather }) => {
   //* Styling for material elements
   const classes = useStyles();
+
+  //* State variables used in search component
+  const [region, setRegion] = useState('');
+  const [state, setState] = useState('');
+  const [city, setCity] = useState('');
+  const [regionList, setRegionList] = useState();
+  const [stateList, setStateList] = useState();
+  const [cityList, setCityList] = useState();
+
   //* Variables used to enable/disable inputs/buttons
   let stateDisabled = !stateList,
     cityDisabled = !cityList,
     btnDisabled = !city;
 
+  // useEffect functions
+  //* Fetch the region list on initial load of the App component
+  useEffect(() => {
+    const fetchRegion = async () => {
+      await fetch(
+        'https://api.countrystatecity.in/v1/countries',
+        requestOptions
+      )
+        .then((response) => response.json())
+        //* Sort the results by Region name
+
+        .then((result) => setRegionList(result))
+        .catch((error) => console.log('error', error));
+    };
+
+    fetchRegion();
+  }, []);
+  //* Fetch the state list after the region has been set
+  useEffect(() => {
+    const fetchState = async () => {
+      await fetch(
+        `https://api.countrystatecity.in/v1/countries/${region}/states`,
+        requestOptions
+      )
+        .then((response) => response.json())
+        //* Sort the results by State name
+
+        .then((result) => setStateList(result))
+        .catch((error) => console.log('error', error));
+    };
+
+    if (region) {
+      fetchState();
+    }
+  }, [region]);
+  //* Fetch the city list after the state has been set
+  useEffect(() => {
+    const fetchCity = async () => {
+      await fetch(
+        `https://api.countrystatecity.in/v1/countries/${region}/states/${state}/cities`,
+        requestOptions
+      )
+        .then((response) => response.json())
+        //* Sort the results by city name
+        .then((result) => setCityList(result))
+        .catch((error) => console.log('error', error));
+    };
+
+    if (state) {
+      fetchCity();
+    }
+  }, [state, region]);
+  //* Fetch the weather data after the lat lng has been retrieved from the Google API
+  useEffect(() => {
+    // Async function to get weather data for the users input location
+    const fetchWeather = async () => {
+      await fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${location.lat}&lon=${location.lng}&exclude=minutely&appid=${process.env.REACT_APP_API_KEY}`
+      )
+        .then((response) => response.json())
+        // Call the function to set the weather state
+        .then((weatherData) => setWeather({ ...weatherData }))
+        // Handle any errors
+        .catch((error) => console.log(`Error: ${error}`));
+    };
+
+    if (location) {
+      fetchWeather();
+    }
+  }, [location]);
+
+  //* onChange functions to set state
+  const handleRegionChange = (e) => {
+    setRegion(e.target.value);
+  };
+  const handleStateChange = (e) => {
+    setState(e.target.value);
+  };
+  const handleCityChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  //* Handle the form submission
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    setCityState({ city, state });
+    // fetch lat lng location from google API
+    getLatLng();
+
+    // Clear the search form fields by clearing the state variables
+    setRegion('');
+    setState('');
+    setCity('');
+  };
+
+  //* Get latitude & longitude from address using google maps geocode API
+  const getLatLng = async () => {
+    await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${city},+${state},+${region}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => data.results[0].geometry.location)
+      .then((latLng) => setLocation({ ...latLng }))
+      .catch((error) => console.error(error));
+  };
+
   return (
-    <Paper elevation={3} className={classes.paper}>
-      <form onSubmit={submitForm}>
-        <Grid
-          container
-          spacing={2}
-          justify='center'
-          align='center'
-          width='auto'
-        >
-          <Grid item sm={12}>
-            <Typography variant='h3' component='h2'>
-              Weather Search
-            </Typography>
-          </Grid>
-          <Grid item sm={12} md={6} lg={3}>
-            <FormControl className={classes.formControl}>
-              <InputLabel id='region-label'>Region</InputLabel>
-              <Select
-                labelId='region-label'
-                id='region'
-                value={region}
-                onChange={handleRegionChange}
-                autoWidth
-                className={classes.selectEmpty}
-              >
-                {/* //* Map through the regions & show them as select inputs */}
-                {regionList.map((region) => (
-                  <MenuItem key={region.id} value={region.iso2}>
-                    {region.name}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item sm={12} md={6} lg={3}>
-            <FormControl className={classes.formControl}>
-              <InputLabel id='state-label'>State</InputLabel>
-              <Select
-                disabled={stateDisabled}
-                labelId='state-label'
-                id='state'
-                value={state}
-                onChange={handleStateChange}
-                autoWidth
-                className={classes.selectEmpty}
-              >
-                {/* //* Map through the states */}
-                {stateList
-                  ? stateList.map((state) => (
-                      <MenuItem key={state.id} value={state.iso2}>
-                        {state.name}
-                      </MenuItem>
-                    ))
-                  : ''}
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item sm={12} md={6} lg={3}>
-            <FormControl className={classes.formControl}>
-              <InputLabel id='city-label'>City</InputLabel>
-              <Select
-                disabled={cityDisabled}
-                labelId='city-label'
-                id='city'
-                value={city}
-                onChange={handleCityChange}
-                autoWidth
-                className={classes.selectEmpty}
-              >
-                {/* //* Map through the cities */}
-                {cityList
-                  ? cityList.map((city) => (
-                      <MenuItem key={city.id} value={city.name}>
-                        {city.name}
-                      </MenuItem>
-                    ))
-                  : ''}
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item sm={12} md={6} lg={3}>
-            <Button
-              disabled={btnDisabled}
-              className={classes.formControl}
-              id='search-btn'
-              variant='contained'
-              type='submit'
+    <div>
+      {/* //* Check if the region list is loaded, if not show loading, if it is show the form */}
+      {!regionList ? (
+        // TODO - Create a loading component while waiting for regions
+        'Loading'
+      ) : (
+        <Paper elevation={3} className={classes.paper}>
+          <form onSubmit={submitForm}>
+            <Grid
+              container
+              spacing={2}
+              justify='center'
+              align='center'
+              width='auto'
             >
-              Search
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
-    </Paper>
+              <Grid item sm={12}>
+                <Typography variant='h3' component='h2'>
+                  Weather Search
+                </Typography>
+              </Grid>
+              <Grid item sm={12} md={6} lg={3}>
+                <FormControl className={classes.formControl}>
+                  <InputLabel id='region-label'>Region</InputLabel>
+                  <Select
+                    labelId='region-label'
+                    id='region'
+                    value={region}
+                    onChange={handleRegionChange}
+                    autoWidth
+                    className={classes.selectEmpty}
+                  >
+                    {/* //* Map through the regions & show them as select inputs */}
+                    {regionList.map((region) => (
+                      <MenuItem key={region.id} value={region.iso2}>
+                        {region.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item sm={12} md={6} lg={3}>
+                <FormControl className={classes.formControl}>
+                  <InputLabel id='state-label'>State</InputLabel>
+                  <Select
+                    disabled={stateDisabled}
+                    labelId='state-label'
+                    id='state'
+                    value={state}
+                    onChange={handleStateChange}
+                    autoWidth
+                    className={classes.selectEmpty}
+                  >
+                    {/* //* Map through the states */}
+                    {stateList
+                      ? stateList.map((state) => (
+                          <MenuItem key={state.id} value={state.iso2}>
+                            {state.name}
+                          </MenuItem>
+                        ))
+                      : ''}
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item sm={12} md={6} lg={3}>
+                <FormControl className={classes.formControl}>
+                  <InputLabel id='city-label'>City</InputLabel>
+                  <Select
+                    disabled={cityDisabled}
+                    labelId='city-label'
+                    id='city'
+                    value={city}
+                    onChange={handleCityChange}
+                    autoWidth
+                    className={classes.selectEmpty}
+                  >
+                    {/* //* Map through the cities */}
+                    {cityList
+                      ? cityList.map((city) => (
+                          <MenuItem key={city.id} value={city.name}>
+                            {city.name}
+                          </MenuItem>
+                        ))
+                      : ''}
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item sm={12} md={6} lg={3}>
+                <Button
+                  disabled={btnDisabled}
+                  className={classes.formControl}
+                  id='search-btn'
+                  variant='contained'
+                  type='submit'
+                >
+                  Search
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Paper>
+      )}
+    </div>
   );
 };
 
